bcb077082540e67239d839d4d8a1bbeb35bde44a
Andrii Reinvald <andreinwald@gmail.com>
2025-05-08 10:25:24 +0200

    tests

---
+++ test/Cache.test.ts
@@ -0,0 +1,27 @@
+import {expect, test, vi} from 'vitest'
+import {cacheFunction} from "../AsyncHookFactory.js";
+
+test('same keys 2 func', async () => {
+    const fn1 = async () => 1;
+    const fn2 = async () => 2;
+    const fn1Cached = cacheFunction(fn1, 10);
+    const fn2Cached = cacheFunction(fn2, 10);
+    await fn1Cached();
+    await fn2Cached();
+    expect(await fn1Cached()).toBe(1);
+    expect(await fn2Cached()).toBe(2);
+});
+
+test('cache and expire', async () => {
+    vi.useFakeTimers();
+    let calls = 0
+    const fn = async () => ++calls;
+    expect(await fn()).toBe(1);
+    expect(await fn()).toBe(2);
+    const fnCached = cacheFunction(fn, 1);
+    expect(await fnCached()).toBe(3);
+    expect(await fnCached()).toBe(3);
+    vi.advanceTimersByTime(1000);
+    expect(await fnCached()).toBe(4);
+    expect(await fnCached()).toBe(4);
+});
